b35989865ca79e71cba2629c7b073245ecfd7bff
Preetesh Verma <verma.preetesh21@gmail.com>
2020-09-19 15:02:44 +0530

    final changes

--- diabetes/app.py
+++ diabetes/app.py
@@ -32,28 +32,27 @@ def predict(X,algo):
     X = np.array(X)
     ss = StandardScaler()
     X = ss.fit_transform(X.reshape(-1, 1))
-    print(X.shape)
+    #print(X.shape)
     ans=(model.predict(X.T))
-    print(ans)
+    #print(ans)
     if(ans==0):
-        print("Healthy :)")
+        #print("Healthy :)")
         return 0
     else:
-        print("Diabetic :(")
+        #print("Diabetic :(")
         return 1
 
-st.title('Diabetes app!!')
-
-
+st.title('Diabetes Prediction App!!')
 
 age=st.number_input('Age',min_value=1,max_value=125)
 bmi=st.number_input('Body Mass Index',min_value=15,max_value=45)
 dpf=st.number_input('Diabetic Pedigree Fraction',min_value=0.0,max_value=5.0)
-preg=st.number_input('Pregnancies',min_value=0,max_value=20,value=1)
-ins=st.number_input('Insulin',min_value=0,max_value=125,value=0)
+preg=st.number_input('Pregnancies(* for males enter 0)',min_value=0,max_value=20,value=1)
+
+ins=st.number_input('Insulin',min_value=0,max_value=250,value=0)
 skin=st.number_input('Skin Thickness',min_value=10,max_value=45)
 glu=st.number_input('Glucose',min_value=60,max_value=325)
-bp=st.number_input('Blood Pressure',min_value=30,max_value=220)
+bp=st.number_input('Blood Pressure(*Enter Diastolic Press. in mm of hg)',min_value=30,max_value=180)
 
 algo=st.radio("Which algo would you prefer",('KNN','SVC','Logisitc Regression','Decision Tree','XGBoost'))  
 
